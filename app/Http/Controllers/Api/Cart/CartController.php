c8990733d7e4ca2d0608b414735382840551888c
Refaat El Sherbiny <refaatelsherbiny38@gmail.com>
2024-12-28 18:18:24 +0200

    first commit test route  server error

--- app/Http/Controllers/Api/Cart/CartController.php
+++ app/Http/Controllers/Api/Cart/CartController.php
@@ -111,7 +111,6 @@ public function update_cart(Request $request)
 public function get_my_cart(Request $request)
 {
    return $OrderProduct = OrderProduct::query()
-        ->with(['product.translations', 'product.wishlists.customer'])
         ->orderBy('id')
         ->orderByDesc('created_at')
         ->whereUser_id(auth()->user()->id)
