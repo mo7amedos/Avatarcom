25eb59c4ce1e2634148fa5598637d9ba9aab9d92
Refaat El Sherbiny <refaatelsherbiny38@gmail.com>
2025-01-02 03:12:42 +0200

    first commit test route  server error

--- app/Http/Controllers/Api/Category/CategoryController.php
+++ app/Http/Controllers/Api/Category/CategoryController.php
@@ -859,7 +859,7 @@ public function update_currency_default(Request $request)
         ], 200, [], JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT);
     }
 
-    $currency->is_default = $request->is_default;
+    $currency->is_default = 'false';
     $currency->save();
 
     return response()->json([
