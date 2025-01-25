0bbef73ce532eb33d91e6d82a5d137738379d68b
Refaat El Sherbiny <refaatelsherbiny38@gmail.com>
2025-01-25 15:22:22 +0200

    form shipment

--- platform/plugins/ecommerce/resources/assets/js/front/checkout.js
+++ platform/plugins/ecommerce/resources/assets/js/front/checkout.js
@@ -260,9 +260,6 @@ class MainCheckout {
             const _self = $(event.currentTarget)
             const $form = _self.closest('form')
 
-            console.log($form);
-            
-
             $.ajax({
                 type: 'POST',
                 cache: false,
