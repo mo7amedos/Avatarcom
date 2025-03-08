2e892d55b3f66f2ac4f1010af2e6325429d0e030
Refaat El Sherbiny <refaatelsherbiny38@gmail.com>
2025-03-08 14:34:41 +0200

    dashboard trainee edit

--- config/payment.php
+++ config/payment.php
@@ -4,7 +4,7 @@
 
 return [
     'secret_key_live' => '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',
-    'secret_key_test' => 'pk_test_51MUHwpAI3k9Off0SUN1XB5r9pwqq9fIrI874fZf3vyvzbfsi8fW0Zh2IVN5aAyO1FlDzUJo3ahQUHaTLVcwSexrY00Z117neP6',
+    'secret_key_test' => 'sk_test_51MUHwpAI3k9Off0SI4cXpN8CuEFAVfl7tbgM7gtvQuqc8l1Rl192Aj5W5HCYml2il3OovqLSWlTmZp0p6vKpSnkw001DHD1dQ2',
 ];
 
 
