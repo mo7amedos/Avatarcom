69fd9fc328cd42e97d744d0ca7bb81487bd4cdd8
Hamada <hamada.emam.tech@gmail.com>
2025-07-29 11:00:50 +0300

    init commit befour update

--- config-old/payment.php
+++ config-old/payment.php
@@ -3,8 +3,8 @@
 
 
 return [
-    'secret_key_live' => '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',
-    'secret_key_test' => 'sk_test_51MUHwpAI3k9Off0SI4cXpN8CuEFAVfl7tbgM7gtvQuqc8l1Rl192Aj5W5HCYml2il3OovqLSWlTmZp0p6vKpSnkw001DHD1dQ2',
+    'secret_key_live' => '',
+    'secret_key_test' => '',
 ];
 
 
